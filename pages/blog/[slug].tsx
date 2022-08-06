26ffef790a6a48dcda4aa831e809308ed2d706c8
galnir <nirnir122@gmail.com>
2022-08-06 12:28:51 +0300

    previews feature

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -70,13 +70,13 @@ export function getStaticPaths() {
  * Posts can come from the fs or our CMS
  */
 
-export async function getStaticProps({ params }) {
+export async function getStaticProps({ params, preview }) {
   let post
   try {
     const filesPath = path.join(process.cwd(), 'posts', params.slug + '.mdx')
     post = fs.readFileSync(filesPath, 'utf-8')
   } catch {
-    const cmsPosts = posts.published.map((post) => {
+    const cmsPosts = (preview ? posts.draft : posts.published).map((post) => {
       return matter(post)
     })
     const match = cmsPosts.find((p) => p.data.slug === params.slug)
